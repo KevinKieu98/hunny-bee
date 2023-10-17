900d2d33d1b30f98f5113527b15fd9cc7da50de5
toankx <toankx@arrowhitech.co>
2023-10-18 00:52:12 +0700

    Update mobile home page

--- assets/hnb-custom.js
+++ assets/hnb-custom.js
@@ -12,20 +12,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
+                        slidesToScroll: 1
                     }
                 },
                 {
-                    breakpoint: 600,
+                    breakpoint: 992,
                     settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToScroll: 1
                     }
                 },
                 {
-                    breakpoint: 480,
+                    breakpoint: 767,
                     settings: {
                         slidesToShow: 1,
                         slidesToScroll: 1
@@ -64,10 +62,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
                     breakpoint: 767,
                     settings: {
                         slidesToShow: 1,
-                        slidesToScroll: 1
+                        slidesToScroll: 1,
+                        dots: true
                     }
                 }
             ]
         })
     }
+
+    // Trigger open search
+    $('.menu_box_search_account .box_search').click(function() {
+        console.log(111111);
+        console.log($('details-modal summary'));
+        $('details-modal summary').trigger('click');
+    });
 });
