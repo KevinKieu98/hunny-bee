0bfc51d92f034201e5b5f3037a9411d751eb46f0
toankx <toankx@arrowhitech.co>
2023-10-22 10:40:00 +0700

    Update product review

--- assets/hnb-custom.js
+++ assets/hnb-custom.js
@@ -80,9 +80,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
             nextArrow: '<button type="button" class="slick-next slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none"><rect width="48" height="48" rx="24" fill="#F7F7F7" fill-opacity="0.8"/><path d="M20 32L28 24L20 16" stroke="#1B1B1B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
             responsive: [
                 {
-                    breakpoint: 1024,
+                    breakpoint: 1200,
                     settings: {
-                        slidesToShow: 4,
+                        slidesToShow: 3,
                         slidesToScroll: 1
                     }
                 },
