a5cbee9eda541b9fb94aa1e1983a576bc9bb668a
toankx <toankx@arrowhitech.co>
2023-12-05 22:02:34 +0700

    make list product in cart up sell

--- assets/hnb-custom.js
+++ assets/hnb-custom.js
@@ -567,7 +567,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     miniCartChange()
     openMiniCart()
+    // End function minicart
 
+    const productMinicartList = $('.minicart_product_list')
+    if (productMinicartList) {
+        productMinicartList.slick({
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            prevArrow: '<button type="button" class="slick-prev slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none"><rect width="32" height="32" rx="16" transform="matrix(-1 0 0 1 32 0)" fill="#F7F7F7"/><path d="M19 22L13 15.5L19 9" stroke="#1B1B1B" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+            nextArrow: '<button type="button" class="slick-next slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none"><rect width="48" height="48" rx="24" fill="#F7F7F7" fill-opacity="0.8"/><path d="M20 32L28 24L20 16" stroke="#1B1B1B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+            dots: true
+        })
+    }
 
 
     if($('.image-zoom')) {
